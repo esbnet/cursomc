d4e975c3317696dc216f77bef4b4ed2e584d5486
esbnet <esbnet@gmail.com>
2018-03-13 17:12:54 -0300

    Estado e Cidade

--- src/main/java/com/esbdev/cursomc/CursomcApplication.java
+++ src/main/java/com/esbdev/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.esbdev.cursomc.domain.Categoria;
+import com.esbdev.cursomc.domain.Cidade;
+import com.esbdev.cursomc.domain.Estado;
 import com.esbdev.cursomc.domain.Produto;
 import com.esbdev.cursomc.repositories.CategoriaRepository;
+import com.esbdev.cursomc.repositories.CidadeRepository;
+import com.esbdev.cursomc.repositories.EstadoRepository;
 import com.esbdev.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,8 +21,15 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 		
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,14 +38,14 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... arg0) throws Exception {
 
-		
+		//Populando Produtos e Categorias
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
+
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat1.getProdutos().addAll(Arrays.asList(p2));
 
@@ -44,6 +55,20 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
+
+		//Populando cidades e estados
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2= new Estado(null,"São Paulo");
+		
+		Cidade c3 = new Cidade(null,"Uberlândia", est1);
+		Cidade c1 = new Cidade(null,"Campinas", est2);
+		Cidade c2 = new Cidade(null,"São Paulo", est2);
+				
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.save(Arrays.asList(est1,est2));
+		cidadeRepository.save(Arrays.asList(c1,c2,c3));
 		
 	}
 }
