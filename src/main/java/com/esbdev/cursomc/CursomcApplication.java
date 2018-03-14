41865b0b6124d0e7354d39c6e30f0e775ee90378
esbnet <esbnet@gmail.com>
2018-03-14 00:34:32 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/esbdev/cursomc/CursomcApplication.java
+++ src/main/java/com/esbdev/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.esbdev.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.esbdev.cursomc.domain.Cidade;
 import com.esbdev.cursomc.domain.Cliente;
 import com.esbdev.cursomc.domain.Endereco;
 import com.esbdev.cursomc.domain.Estado;
+import com.esbdev.cursomc.domain.Pagamento;
+import com.esbdev.cursomc.domain.PagamentoComBoleto;
+import com.esbdev.cursomc.domain.PagamentoComCartao;
+import com.esbdev.cursomc.domain.Pedido;
 import com.esbdev.cursomc.domain.Produto;
+import com.esbdev.cursomc.domain.enums.EstadoPagamento;
 import com.esbdev.cursomc.domain.enums.TipoCliente;
 import com.esbdev.cursomc.repositories.CategoriaRepository;
 import com.esbdev.cursomc.repositories.CidadeRepository;
 import com.esbdev.cursomc.repositories.ClienteRepository;
 import com.esbdev.cursomc.repositories.EnderecoRepository;
 import com.esbdev.cursomc.repositories.EstadoRepository;
+import com.esbdev.cursomc.repositories.PagamentoRepository;
+import com.esbdev.cursomc.repositories.PedidoRepository;
 import com.esbdev.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -35,13 +43,19 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-		
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-		
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-		
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -49,54 +63,69 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... arg0) throws Exception {
 
-		//Populando Produtos e Categorias
+		// Populando Produtos e Categorias
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat1.getProdutos().addAll(Arrays.asList(p2));
 
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
 
-		//Populando cidades e estados
-		Estado est1 = new Estado(null,"Minas Gerais");
-		Estado est2= new Estado(null,"São Paulo");
-		
-		Cidade c3 = new Cidade(null,"Uberlândia", est1);
-		Cidade c1 = new Cidade(null,"Campinas", est2);
-		Cidade c2 = new Cidade(null,"São Paulo", est2);
-				
+		// Populando cidades e estados
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c3 = new Cidade(null, "Uberlândia", est1);
+		Cidade c1 = new Cidade(null, "Campinas", est2);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+
 		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
-		
-		estadoRepository.save(Arrays.asList(est1,est2));
-		cidadeRepository.save(Arrays.asList(c1,c2,c3));
-		
-		//Cliente, endereço, telefone
-		
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.save(Arrays.asList(est1, est2));
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
+
+		// Cliente, endereço, telefone
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "50930877500", TipoCliente.PESSOA_FISICA);
-		cli1.getTelefones().addAll(Arrays.asList("33931676","88630845"));
-		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Ap. 303", "Jadim","50930877500" ,cli1, c1);
-		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro","50930877500" ,cli1, c2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
-		
+		cli1.getTelefones().addAll(Arrays.asList("33931676", "88630845"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Ap. 303", "Jadim", "50930877500", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "50930877500", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
 		clienteRepository.save(Arrays.asList(cli1));
-		enderecoRepository.save(Arrays.asList(e1,e2));
-		
-		
-		
-		
-		
+		enderecoRepository.save(Arrays.asList(e1, e2));
+
+		// Pedido, paramento, forma de pagamento
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));
+
 	}
 }
