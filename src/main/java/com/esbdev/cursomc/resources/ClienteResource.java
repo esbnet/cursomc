5de05f1b5832e3470a1f9b45e616eebb482f3de0
esbnet <esbnet@gmail.com>
2018-05-02 18:15:40 -0300

    Incluindo um novo Cliente com POST

--- src/main/java/com/esbdev/cursomc/resources/ClienteResource.java
+++ src/main/java/com/esbdev/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.esbdev.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.esbdev.cursomc.domain.Cliente;
 import com.esbdev.cursomc.dto.ClienteDTO;
+import com.esbdev.cursomc.dto.ClienteNewDTO;
 import com.esbdev.cursomc.services.ClienteService;
 
 @RestController
@@ -25,6 +28,14 @@ public class ClienteResource {
 	
 	@Autowired
 	private ClienteService service;
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@RequestMapping(value="/{id}",  method=RequestMethod.GET)
 	public  ResponseEntity<Cliente> find(@PathVariable Integer id)	{
--- src/main/java/com/esbdev/cursomc/services/ClienteService.java
+++ src/main/java/com/esbdev/cursomc/services/ClienteService.java
@@ -8,10 +8,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.esbdev.cursomc.domain.Cidade;
 import com.esbdev.cursomc.domain.Cliente;
+import com.esbdev.cursomc.domain.Endereco;
+import com.esbdev.cursomc.domain.enums.TipoCliente;
 import com.esbdev.cursomc.dto.ClienteDTO;
+import com.esbdev.cursomc.dto.ClienteNewDTO;
+import com.esbdev.cursomc.repositories.CidadeRepository;
 import com.esbdev.cursomc.repositories.ClienteRepository;
+import com.esbdev.cursomc.repositories.EnderecoRepository;
 import com.esbdev.cursomc.services.exceptions.DataIntegrityException;
 import com.esbdev.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -20,6 +27,12 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Cliente obj = repo.findOne(id);
@@ -30,9 +43,12 @@ public class ClienteService {
 		return obj;
 	}
 
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj = repo.save(obj);
+		enderecoRepository.save(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -69,6 +85,23 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 
 }
