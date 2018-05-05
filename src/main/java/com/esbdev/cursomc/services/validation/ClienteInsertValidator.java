19dfcc7a7504cd61a71befdd783a2d7a0b6261a5
esbnet <esbnet@gmail.com>
2018-05-05 14:53:58 -0300

    Validação customizada de CPF e CNPJ na insercao do cliente

---
+++ src/main/java/com/esbdev/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,40 @@
+package com.esbdev.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.esbdev.cursomc.domain.enums.TipoCliente;
+import com.esbdev.cursomc.dto.ClienteNewDTO;
+import com.esbdev.cursomc.resources.exception.FieldMessage;
+import com.esbdev.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAL_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFildName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
