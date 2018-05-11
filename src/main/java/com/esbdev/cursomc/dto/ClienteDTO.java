61072302a80a70fbc43973e03cf65898f6a46167
esbnet <esbnet@gmail.com>
2018-05-11 00:41:40 -0300

    Validação customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/esbdev/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/esbdev/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,10 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.esbdev.cursomc.domain.Cliente;
+import com.esbdev.cursomc.services.validation.ClienteUpdate;
 
+
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/esbdev/cursomc/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/esbdev/cursomc/services/validation/ClienteUpdateValidator.java
@@ -2,45 +2,47 @@ package com.esbdev.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.esbdev.cursomc.domain.Cliente;
-import com.esbdev.cursomc.domain.enums.TipoCliente;
-import com.esbdev.cursomc.dto.ClienteNewDTO;
+import com.esbdev.cursomc.dto.ClienteDTO;
 import com.esbdev.cursomc.repositories.ClienteRepository;
 import com.esbdev.cursomc.resources.exception.FieldMessage;
-import com.esbdev.cursomc.services.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
 
+	@Autowired
+	private HttpServletRequest request;
+	
+	
 	@Autowired
 	private ClienteRepository repo;
 
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 
+		//Pega id da URI Json
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriID = Integer.parseInt(map.get("id"));
+		
 		List<FieldMessage> list = new ArrayList<>();
-
-		if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
-		}
-
-		if (objDto.getTipo().equals(TipoCliente.PESSOAL_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
-		}
 
 		// Verifica se e-mail já existe
 		Cliente aux = repo.findByEmail(objDto.getEmail());
 
-		if (aux != null) {
+		if (aux != null && !aux.getId().equals(uriID)) {
 			list.add(new FieldMessage("Email", "Email já cadastrado"));
 		}
 
