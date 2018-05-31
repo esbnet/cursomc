1ea7d4097b18471f39f7f75390c38df8abb216fc
esbnet <esbnet@gmail.com>
2018-05-31 18:08:36 -0300

    Adicionand  senha ao Cliente

--- src/main/java/com/esbdev/cursomc/services/DBService.java
+++ src/main/java/com/esbdev/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.esbdev.cursomc.domain.Categoria;
@@ -32,6 +33,9 @@ import com.esbdev.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -109,7 +113,7 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "esbnet@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "esbnet@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
