285e4d3d41b20eb276ded438568403d81705a942
esbnet <esbnet@gmail.com>
2018-06-19 19:58:48 -0300

    Esqueci a senha

--- src/main/java/com/esbdev/cursomc/services/MockEmailService.java
+++ src/main/java/com/esbdev/cursomc/services/MockEmailService.java
@@ -1,15 +1,18 @@
 package com.esbdev.cursomc.services;
 
+
 import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.esbdev.cursomc.domain.Pedido;
+
 public class MockEmailService extends AbstractEmailService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
-
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email...");
@@ -18,10 +21,14 @@ public class MockEmailService extends AbstractEmailService {
 	}
 
 	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
 	public void sendHtmlEmail(MimeMessage msg) {
-		LOG.info("Simulando envio de email Html...");
-		LOG.info(msg.toString());
-		LOG.info("Email enviado");
+		// TODO Auto-generated method stub
 		
 	}
 }
